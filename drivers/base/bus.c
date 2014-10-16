eaba93160753e7a9853d37390d8bb3c24ede5dc8
Frans Klaver <fransklaver@gmail.com>
2014-10-16 23:55:26 +0200

    base: bus: use BUS_ATTR_*
    Use the BUS_ATTR_* API. This leaves less interpretation to the reader.
    Also rename the store and show functions while we're at it.
    
    Signed-off-by: Frans Klaver <fransklaver@gmail.com>

--- drivers/base/bus.c
+++ drivers/base/bus.c
@@ -235,12 +235,12 @@ static ssize_t bind_store(struct device_driver *drv, const char *buf,
 }
 static DRIVER_ATTR_WO(bind);
 
-static ssize_t show_drivers_autoprobe(struct bus_type *bus, char *buf)
+static ssize_t drivers_autoprobe_show(struct bus_type *bus, char *buf)
 {
 	return sprintf(buf, "%d\n", bus->p->drivers_autoprobe);
 }
 
-static ssize_t store_drivers_autoprobe(struct bus_type *bus,
+static ssize_t drivers_autoprobe_store(struct bus_type *bus,
 				       const char *buf, size_t count)
 {
 	if (buf[0] == '0')
@@ -250,7 +250,7 @@ static ssize_t store_drivers_autoprobe(struct bus_type *bus,
 	return count;
 }
 
-static ssize_t store_drivers_probe(struct bus_type *bus,
+static ssize_t drivers_probe_store(struct bus_type *bus,
 				   const char *buf, size_t count)
 {
 	struct device *dev;
@@ -621,9 +621,8 @@ static void remove_bind_files(struct device_driver *drv)
 	driver_remove_file(drv, &driver_attr_unbind);
 }
 
-static BUS_ATTR(drivers_probe, S_IWUSR, NULL, store_drivers_probe);
-static BUS_ATTR(drivers_autoprobe, S_IWUSR | S_IRUGO,
-		show_drivers_autoprobe, store_drivers_autoprobe);
+static BUS_ATTR_WO(drivers_probe);
+static BUS_ATTR_RW(drivers_autoprobe);
 
 static int add_probe_files(struct bus_type *bus)
 {
@@ -849,8 +848,8 @@ static void klist_devices_put(struct klist_node *n)
 	put_device(dev);
 }
 
-static ssize_t bus_uevent_store(struct bus_type *bus,
-				const char *buf, size_t count)
+static ssize_t uevent_store(struct bus_type *bus,
+			    const char *buf, size_t count)
 {
 	enum kobject_action action;
 
@@ -858,7 +857,7 @@ static ssize_t bus_uevent_store(struct bus_type *bus,
 		kobject_uevent(&bus->p->subsys.kobj, action);
 	return count;
 }
-static BUS_ATTR(uevent, S_IWUSR, NULL, bus_uevent_store);
+static BUS_ATTR_WO(uevent);
 
 /**
  * bus_register - register a driver-core subsystem
